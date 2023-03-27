c64c1e8511dece18bfc9675cf7975f13e145e273
moonki kim <moonki0623@naver.com>
2023-03-27 14:46:37 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
+
+// GlobalStyle
+import GlobalStyle from "./styles/GlobalStyle.js";
+
+// RecoilStates
+import { clickedIconState, iconPositionState } from "./states/atom.js";
+
 function App() {
+  const [clickedIcon, setClikedIcon] = useRecoilState(clickedIconState);
+  const [iconPosition, setIconPosition] = useRecoilState(iconPositionState);
+
   return (
     <div>
+      <GlobalStyle />
       React Messanger
     </div>
   );
