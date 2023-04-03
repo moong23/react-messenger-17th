e6815af6df04077a7145b8e5c850b6d170e03e7a
moonki0623@ninewatt.com <moonki0623@naver.com>
2023-04-03 21:12:34 +0900

    [feat]: uuid로 key값 강화

--- src/states/atom.tsx
+++ src/states/atom.tsx
@@ -1,4 +1,5 @@
 import { atom } from "recoil";
+import { v4 } from "uuid";
 
 interface IconPosition {
   top: number;
@@ -6,7 +7,7 @@ interface IconPosition {
 }
 
 export const TodoIconPositionState = atom<IconPosition>({
-  key: "todoIconPositionState",
+  key: `todoIconPositionState${v4()}`,
   default: JSON.parse(localStorage.getItem("iconPosition") || "null") ?? {
     top: 30,
     left: 30,
@@ -14,24 +15,41 @@ export const TodoIconPositionState = atom<IconPosition>({
 });
 
 export const MessengerIconPositionState = atom<IconPosition>({
-  key: "messengerIconPositionState",
+  key: `messengerIconPositionState${v4()}`,
   default: JSON.parse(localStorage.getItem("iconPosition") || "null") ?? {
     top: 160,
     left: 30,
   },
 });
 
-export const clickedIconState = atom({
-  key: "clickedIconState",
+export const clickedIconState = atom<string>({
+  key: `clickedIconState${v4()}`,
   default: "",
 });
 
-export const selectedTagState = atom({
-  key: "selectedTagState",
+export const todoOpenState = atom<boolean>({
+  key: `todoOpenState${v4()}`,
+  default: false,
+});
+
+export const messengerOpenState = atom<boolean>({
+  key: `messengerOpenState${v4()}`,
+  default: false,
+});
+
+export const priorityState = atom<"todo" | "messenger">({
+  key: `priorityState${v4()}`,
+  default: "todo",
+});
+
+export const selectedTagState = atom<
+  "red" | "blue" | "orange" | "yellow" | "green" | "purple"
+>({
+  key: `selectedTagState${v4()}`,
   default: "red",
 });
 
 export const allListState = atom({
-  key: "allListState",
+  key: `allListState${v4()}`,
   default: JSON.parse(localStorage.getItem("todoList") || "null") ?? [],
 });
